9d021abc475d068c4010ec6f0a4d60bae3fb882c
Damien Martin <martin.damien.pierre@gmail.com>
2014-10-04 16:21:40 +0200

    [TDD] authentication hash for request

--- sponsorPay/Models/OfferRequest.swift
+++ sponsorPay/Models/OfferRequest.swift
@@ -10,23 +10,62 @@ import UIKit
 
 class OfferRequest: NSObject {
 	
+	/* private */
+	private var apiKey: String = defaultApiKey;
+	
 	/* request Params */
-	var format: String =  defaultFormat
-	var appid: String =  defaultAppId
-	var uid: String = defaultUid
-	var locale: String = defaultLocale
-	var ip: String = defaultIp
-	var offerTypes: String = defaultOfferTypes;
-	var appleIdfa: String {
-		get {
-			return "TEST"
-		}
-	}
+	var format: String? =  defaultFormat
+	var appid: String? =  defaultAppId
+	var uid: String? = defaultUid
+	var locale: String? = defaultLocale
+	var ip: String? = defaultIp
+	var offerTypes: String? = defaultOfferTypes;
+	var appleIdfa: String?
+	var deviceId: String?
+	var page: String?
+	var ps_time: String?
+	var pub0: String?
+	var timestamp: String?
 	
 	override init() {
 		super.init();
 	}
 	
+	/// Return ordered URL params (except hash)
+	func orderedParams() -> NSArray {
+		var params: NSMutableDictionary = NSMutableDictionary();
+		if self.format != nil { params.setObject(self.format!, forKey: "format") }
+		if self.appid != nil { params.setObject(self.appid!, forKey: "appid") }
+		if self.uid != nil { params.setObject(self.uid!, forKey: "uid") }
+		if self.locale != nil { params.setObject(self.locale!, forKey: "locale") }
+		if self.ip != nil { params.setObject(self.ip!, forKey: "ip") }
+		if self.offerTypes != nil { params.setObject(self.offerTypes!, forKey: "offer_types") }
+		if self.appleIdfa != nil { params.setObject(self.appleIdfa!, forKey: "apple_idfa") }
+		if self.deviceId != nil { params.setObject(self.deviceId!, forKey: "device_id") }
+		if self.page != nil { params.setObject(self.page!, forKey: "page") }
+		if self.ps_time != nil { params.setObject(self.ps_time!, forKey: "ps_time") }
+		if self.pub0 != nil { params.setObject(self.pub0!, forKey: "pub0") }
+		if self.timestamp != nil { params.setObject(self.timestamp!, forKey: "timestamp") }
+	
+
+		var paramsToReturn: NSMutableArray = NSMutableArray();
+		
+		let allKeys: [String] = params.allKeys as [String];
+		let allKeysOrdered = allKeys.sorted { (s1, s2) -> Bool in
+			return s1 < s2
+		}
+		
+		for key in allKeysOrdered as [String] {
+			if let value = params.objectForKey(key) as? String {
+				let paramToAdd: NSDictionary = NSDictionary(object: value, forKey: key);
+				paramsToReturn.addObject(paramToAdd);
+			}
+		}
+		
+		return paramsToReturn
+	}
+	
+	/// Return concat URL params string (except hash)
 	func requestParamsValue() -> String {
 		let paramsOrdered = self.orderedParams();
 		
@@ -48,32 +87,15 @@ class OfferRequest: NSObject {
 		return paramsValue;
 	}
 	
-	func orderedParams() -> NSArray {
-		var params: NSMutableDictionary = NSMutableDictionary();
-		params.setObject(self.format, forKey: "format");
-		params.setObject(self.appid, forKey: "appid");
-		params.setObject(self.uid, forKey: "uid");
-		params.setObject(self.locale, forKey: "locale");
-		params.setObject(self.ip, forKey: "ip");
-		params.setObject(self.offerTypes, forKey: "offer_types");
-		params.setObject(self.appleIdfa, forKey: "apple_idfa");
-
+	func authenticationHash() -> String {
+		let paramsValue = self.requestParamsValue();
+		let valueToHash: NSString = "\(paramsValue)&\(self.apiKey)" as NSString
 		
-		var paramsToReturn: NSMutableArray = NSMutableArray();
-		
-		let allKeys: [String] = params.allKeys as [String];
-		let allKeysOrdered = allKeys.sorted { (s1, s2) -> Bool in
-			return s1 < s2
-		}
-		
-		for key in allKeysOrdered as [String] {
-			if let value = params.objectForKey(key) as? String {
-				let paramToAdd: NSDictionary = NSDictionary(object: value, forKey: key);
-				paramsToReturn.addObject(paramToAdd);
-			}
-		}
-		
-		return paramsToReturn
+		return valueToHash.sha1;
 	}
 	
+	/// Only use for test
+	func setApiKey(apiKey newApiKey:String) {
+		apiKey = newApiKey;
+	}
 }
--- sponsorPayTests/offerRequestTests.swift
+++ sponsorPayTests/offerRequestTests.swift
@@ -53,12 +53,41 @@ class offerRequestTests: XCTestCase {
 		let requestParamsValue = self.request!.requestParamsValue();
 	
 		XCTAssertFalse(requestParamsValue.isEmpty, "requestParamsValue is Empty")
+		println("\(requestParamsValue)")
 		
-		let defaultValue = "appid=2070&apple_idfa=TEST&format=json&ip=109.235.143.113&locale=DE&offer_types=112&uid=spiderman"
-		XCTAssert( (requestParamsValue == defaultValue), "requestParamsValue is Empty")
+		let defaultValue = "appid=2070&format=json&ip=109.235.143.113&locale=DE&offer_types=112&uid=spiderman"
+		XCTAssert( (requestParamsValue == defaultValue), "requestParamsValue is wrong \(requestParamsValue) != \(defaultValue)")
 		
+	}
+	
+	func testAuthenticationHash() {
+		// Doc Example hash
+		request!.appid = "157";
+		request!.deviceId = "2b6f0cc904d137be2e1730235f5664094b831186"
+		request!.ip = "212.45.111.17"
+		request!.locale = "de"
+		request!.page = "2"
+		request!.ps_time = "1312211903"
+		request!.pub0 = "campaign2"
+		request!.timestamp = "1312553361"
+		request!.uid = "player1"
+		request!.offerTypes = nil;
+		request!.format = nil;
+		request!.appleIdfa = nil;
+			
+		request!.setApiKey(apiKey: "e95a21621a1865bcbae3bee89c4d4f84")
 		
+		let paramsValue = request!.requestParamsValue()
+		let normalParamsValue = "appid=157&device_id=2b6f0cc904d137be2e1730235f5664094b831186&ip=212.45.111.17&locale=de&page=2&ps_time=1312211903&pub0=campaign2&timestamp=1312553361&uid=player1"
 		
+		XCTAssert( (paramsValue == normalParamsValue), "paramsValue not good : \(paramsValue) != \(normalParamsValue)");
+		
+	
+		let hash = self.request!.authenticationHash();
+		XCTAssertFalse( hash.isEmpty, "Hash is empty");
+		
+		let testHash = "7a2b1604c03d46eec1ecd4a686787b75dd693c4d"
+		XCTAssert( (hash == testHash), "Wrong hash : \(hash) != \(testHash)" );
 	}
 
 }
