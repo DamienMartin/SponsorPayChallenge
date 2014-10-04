d905d0b5a65242fb5ebec7b7b93d2b789a2a29ee
Damien Martin <martin.damien.pierre@gmail.com>
2014-10-04 13:28:25 +0200

    [TDD] orderdedParams not return empty Array

--- sponsorPay/Models/OfferRequest.swift
+++ sponsorPay/Models/OfferRequest.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 class OfferRequest: NSObject {
-   
+	
+	/* request Params */
+	var format: String =  defaultFormat
+	var appid: String =  defaultAppId
+	var uid: String = defaultUid
+	var locale: String = defaultLocale
+	var ip: String = defaultIp
+	var offerTypes: String = defaultOfferTypes;
+	var apple_idfa: String?
+	
+	override init() {
+		super.init();
+	}
+	
+	func orderedParams() -> NSArray {
+		var params: NSMutableDictionary = NSMutableDictionary();
+		params.setObject(self.format, forKey: "format");
+		
+		var paramsToReturn: NSMutableArray = NSMutableArray();
+		
+		for key in params.allKeys as [String] {
+			if let value = params.objectForKey(key) as? String {
+				let paramToAdd: NSDictionary = NSDictionary(object: value, forKey: key);
+				paramsToReturn.addObject(paramToAdd);
+			}
+		}
+		
+		return paramsToReturn
+	}
+	
 }
---
+++ sponsorPay/constants.swift
@@ -0,0 +1,18 @@
+//
+//  constants.swift
+//  sponsorPay
+//
+//  Created by Damien Martin on 04/10/2014.
+//  Copyright (c) 2014 Damien Martin. All rights reserved.
+//
+
+import Foundation
+
+let defaultFormat: String = "json"
+let defaultAppId: String = "2070"
+let defaultUid: String = "spiderman"
+let defaultLocale: String = "DE"
+let defaultIp: String = "109.235.143.113"
+let defaultOfferTypes: String = "112"
+let defaultApiKey: String = "1c915e3b5d42d05136185030892fbb846c278927"
+let defaultPub0 = "campaign2"
