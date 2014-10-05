65adac56a4fe42d0302635d4d569a8fb9244cf1c
Damien Martin <martin.damien.pierre@gmail.com>
2014-10-05 19:36:05 +0200

    refactoring + Improve test

--- sponsorPay/Controllers/FormViewController.swift
+++ sponsorPay/Controllers/FormViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, OfferFormViewDelegate {
+class FormViewController: UIViewController, OfferFormViewDelegate {
 
 	@IBOutlet var form: OfferFormView?
 	
